2447d9f713f0f4f7d58cf504af432d6f7619d144
OmriChenYosef <OmriChenYosef@GMAIL.COM>
2025-01-16 05:50:14 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -19,7 +19,7 @@ function App() {
                  <Card title="The team" text="text" imgSrc="https://fzstage.ssf.ae/upload/vouchers/img-world-including-5-vat-1700564667144.jpg" />
                   <Card title="The team" text="text" imgSrc="https://fzstage.ssf.ae/upload/vouchers/img-world-including-5-vat-1700564667144.jpg" />
                  <Card title="The team" text="text" imgSrc="https://fzstage.ssf.ae/upload/vouchers/img-world-including-5-vat-1700564667144.jpg" />
-              
+                 <Card title="The team" text="text" imgSrc="https://fzstage.ssf.ae/upload/vouchers/img-world-including-5-vat-1700564667144.jpg" />
                 </div>
                 <Footer />
            </div>
